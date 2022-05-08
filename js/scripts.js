5123f023a783c0873491790e70248c673475d21a
OLGON92 <ogon318@gmail.com>
2022-05-07 19:31:50 -0700

    add the function for pizza.

--- js/scripts.js
+++ js/scripts.js
@@ -3,3 +3,23 @@ function Pizza(size, toppings) {
   this.pizzaSize = size;
   this.pizzaToppings = toppings;
 }
+
+Pizza.prototype.cost = function(){
+  let totalCost = 0;
+
+  const pizzaSize = new Map ([
+    ["Small", 8.00],
+    ["Medium", 14.00],
+    ["Large", 18.00]
+  ]); 
+
+  const pizzaToppings = new Map ([
+    ["Artisanal Salami", 4.00],
+    ["Sausage", 3.00],
+    ["Pepperoni", 3.00],
+    ["Mushroom", 2.00],
+    ["Bell-Peppers", 2.00],
+    ["Onions", 1.00]
+  ]);
+this.pizzaToppings.forEach(function(element) {
+
