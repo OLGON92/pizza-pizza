d7df27c6330348ac2d73159cec3db478ba9ad340
OLGON92 <ogon318@gmail.com>
2022-05-07 19:51:45 -0700

    add the final part of the business logic.

--- js/scripts.js
+++ js/scripts.js
@@ -21,5 +21,10 @@ Pizza.prototype.cost = function(){
     ["Bell-Peppers", 2.00],
     ["Onions", 1.00]
   ]);
-this.pizzaToppings.forEach(function(element)
+this.pizzaToppings.forEach(function(element) {
+  totalCost += pizzaToppings.get(element)
+});
+price = pizzaSize.get(this.pizzaSize) + totalCost;
+return price;
+} 
 
